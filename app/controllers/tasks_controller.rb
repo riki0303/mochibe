2858ebb07e6ec753d84699004c984b8a671577d8
小久保力輝 <kokub0303@gmail.com>
2024-02-18 15:37:26 +0900

    task create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,5 +6,18 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      redirect_to root_path, notice: '記録出来ました'
+    else
+      flash.now[:alert] = '記録出来ませんでした'
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :date, :time, :memo)
   end
 end
