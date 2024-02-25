f244ce4f8eb5d57a89a4eabb2d7f7bc2a2c11c8f
小久保力輝 <kokub0303@gmail.com>
2024-02-25 22:44:27 +0900

    勉強時間の合計を表示

---
+++ app/helpers/tasks_helper.rb
@@ -0,0 +1,16 @@
+module TasksHelper
+
+  def task_time_today
+    @task.all.sum(:time)
+  end
+
+  def task_time_last_week
+    tasks_week = @task.where(created_at: 1.week.ago..Time.now)
+    tasks_week.sum(:time)
+  end
+
+  def task_time_last_month
+    tasks_week = @task.where(created_at: 30.week.ago..Time.now)
+    tasks_week.sum(:time)
+  end
+end
