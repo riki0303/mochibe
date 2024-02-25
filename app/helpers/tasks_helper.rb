7c3280adbe7673d607e1bd90b8415dbf46e21d72
小久保力輝 <kokub0303@gmail.com>
2024-02-25 22:47:27 +0900

    メソッド名変更

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -4,12 +4,12 @@ module TasksHelper
     @task.all.sum(:time)
   end
 
-  def task_time_last_week
+  def task_time_week
     tasks_week = @task.where(created_at: 1.week.ago..Time.now)
     tasks_week.sum(:time)
   end
 
-  def task_time_last_month
+  def task_time_month
     tasks_week = @task.where(created_at: 30.week.ago..Time.now)
     tasks_week.sum(:time)
   end
