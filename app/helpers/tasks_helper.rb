38030c2710aa77da2866b769be91b43803f21954
小久保力輝 <kokub0303@gmail.com>
2024-02-29 12:22:50 +0900

    今日分の勉強時間追加＋日時を厳密に設定

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,16 +1,23 @@
 module TasksHelper
 
   def task_time_today
-    @task.all.sum(:time)
+    today = Date.today
+    tasks_today = @task.where(created_at: today.beginning_of_day..today.end_of_day)
+    tasks_today.sum(:time)
   end
 
   def task_time_week
-    tasks_week = @task.where(created_at: 1.week.ago..Time.now)
+    today = Date.today
+    tasks_week = @task.where(created_at: 1.week.ago.beginning_of_day..today.end_of_day)
     tasks_week.sum(:time)
   end
 
   def task_time_month
-    tasks_week = @task.where(created_at: 30.week.ago..Time.now)
-    tasks_week.sum(:time)
+    today = Date.today
+    tasks_month = @task.where(created_at: 30.week.ago.beginning_of_day..today.end_of_day)
+    tasks_month.sum(:time)
+  end
+  def task_time_all
+    @task.all.sum(:time)
   end
 end
