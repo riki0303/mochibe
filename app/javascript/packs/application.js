c95e4e55bfd9bb995e28da57f528a9fc9e997b00
小久保力輝 <kokub0303@gmail.com>
2024-03-02 13:26:00 +0900

    install chart.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require('@rails/ujs').start();
+require('turbolinks').start();
+require('@rails/activestorage').start();
+require('channels');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +14,31 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import Chart from 'chart.js/auto';
+
+document.addEventListener('turbolinks:load', () => {
+  (async function () {
+    const data = [
+      { year: 2010, count: 10 },
+      { year: 2011, count: 20 },
+      { year: 2012, count: 15 },
+      { year: 2013, count: 25 },
+      { year: 2014, count: 22 },
+      { year: 2015, count: 30 },
+      { year: 2016, count: 28 },
+    ];
+
+    new Chart(document.getElementById('acquisitions'), {
+      type: 'bar',
+      data: {
+        labels: data.map((row) => row.year),
+        datasets: [
+          {
+            label: '学習時間',
+            data: data.map((row) => row.count),
+          },
+        ],
+      },
+    });
+  })();
+});
